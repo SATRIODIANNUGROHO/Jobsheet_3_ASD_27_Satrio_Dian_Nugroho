f77436b0d7f8781742dcc53688922ed66085d925
Satrio Dian Nugroho <143977344+SATRIODIANNUGROHO@users.noreply.github.com>
2024-02-29 01:00:49 +0700

    Delete PersegiPanjang.java

--- PersegiPanjang.java
+++
@@ -1,9 +0,0 @@
-public class PersegiPanjang {
-    public int panjang;
-    public int lebar;
-    public int urutan;
-    public void cetakInfo() {
-        System.out.println(", panjang : " + (panjang) 
-        + ", lebar : " + (lebar));
-    }
-}
