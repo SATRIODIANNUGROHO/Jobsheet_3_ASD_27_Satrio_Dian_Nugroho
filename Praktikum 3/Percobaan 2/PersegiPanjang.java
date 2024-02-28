d4786ef283f3c1cc74155c7be28de8020a58067f
Satrio Dian Nugroho <143977344+SATRIODIANNUGROHO@users.noreply.github.com>
2024-02-29 01:28:32 +0700

    Percobaan 2

---
+++ Praktikum 3/Percobaan 2/PersegiPanjang.java
@@ -0,0 +1,8 @@
+public class PersegiPanjang {
+    public int panjang;
+    public int lebar;
+    public void cetakInfo() {
+        System.out.println(", panjang : " + (panjang) 
+        + ", lebar : " + (lebar));
+    }
+}
